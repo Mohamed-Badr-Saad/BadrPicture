f57e581516d7b99cdc3103fee144cffaae26657e
Mohamed Badr <badrm7103@gmail.com>
2025-06-13 20:50:24 +0300

    Saved page modification (no saved posts)

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -12,6 +12,13 @@ const Saved = () => {
       <div className="explore-inner_container">
         <h2 className="h3-bold md:h2-bold w-full">Saved Posts</h2>
       </div>
+
+      {savedPosts?.documents.length === 0 && (
+        <div className="flex items-center justify-center h-80">
+          <p className="text-light-4">No saved posts</p>
+        </div>
+      )}
+      
       <GridPostList
         posts={savedPosts?.documents || []}
         savedPostsInput={true}
